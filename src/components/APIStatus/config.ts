26cf066ad98181c0924471af71753e22afe54f82
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2024-12-23 08:55:07 +0000

    1

--- src/components/APIStatus/config.ts
+++ src/components/APIStatus/config.ts
@@ -1,6 +1,6 @@
 export const healthRecords: HealthMonitor.HealthRecord = {
   游戏管理:
-    "https://status.ltsc.vip/monitor/clnzoxcy10001vy2ohi4obbi0/cm26dov5e000z12mywglwp5q5/badge.svg",
+    "https://status.ltsc.vip/monitor/clnzoxcy10001vy2ohi4obbi0/cm50pgv0r01fc38mwr7zski3d/badge.svg",
   主控中心:
     "https://status.ltsc.vip/monitor/clnzoxcy10001vy2ohi4obbi0/cm4zsgrx4000138mwl7ukyby9/badge.svg",
   智能问答:
@@ -12,5 +12,5 @@ export const healthRecords: HealthMonitor.HealthRecord = {
   QQ机器人:
     "https://status.ltsc.vip/monitor/clnzoxcy10001vy2ohi4obbi0/cm50sb1hg01oe38mwpfhg4sza/badge.svg",
   授权服务:
-    "https://status.ltsc.vip/monitor/clnzoxcy10001vy2ohi4obbi0/cm26dov5e000z12mywglwp5q5/badge.svg",
+    "https://status.ltsc.vip/monitor/clnzoxcy10001vy2ohi4obbi0/cm50pgv0r01fc38mwr7zski3d/badge.svg",
 };
