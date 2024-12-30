b8f2db9004c6c7aecec4e964f623be2e87e0f74e
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2024-12-30 14:09:13 +0000

    1

--- src/views/user/Dashboard.vue
+++ src/views/user/Dashboard.vue
@@ -55,7 +55,7 @@
                                     @click.stop="handleDeleteBtnOnClick(slot.uuid, slot.gameAccount)">删除</button>
                             </div>
                         </div>
-                        <div v-if="!findGame(slot.gameAccount)">
+                        <div v-if="!findGame(slot.gameAccount) && isGameListCompletedInit">
                             <button :disabled="isLoading" class="btn btn-outline btn-sm btn-block btn-error mt-2"
                                 @click.stop="handleRepairBtnOnClick(slot.uuid, slot.gameAccount)">点击进行修复</button>
                         </div>
